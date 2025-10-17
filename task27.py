ae09bc8c25129bf7d4a531436aef5b068c88c0e2
lizashtokareva <liza.viplab@gmail.com>
2025-10-17 19:17:17 +0300

    Add files via upload

---
+++ task27.py
@@ -0,0 +1,46 @@
+#todo: Числа в буквы
+#Замените числа, написанные через пробел, на буквы. Не числа не изменять.
+
+#Пример.
+#Input	                            Output
+#8 5 12 12 15	                    hello
+#8 5 12 12 15 , 0 23 15 18 12 4 !	hello, world!#
+
+numbers_to_letters = {
+    '1': 'a',
+    '2': 'b',
+    '3': 'c',
+    '4': 'd',
+    '5': 'e',
+    '6': 'f',
+    '7': 'g',
+    '8': 'h',
+    '9': 'i',
+    '10': 'j',
+    '11': 'k',
+    '12': 'l',
+    '13': 'm',
+    '14': 'n',
+    '15': 'o',
+    '16': 'p',
+    '17': 'q',
+    '18': 'r',
+    '19': 's',
+    '20': 't',
+    '21': 'u',
+    '22': 'v',
+    '23': 'w',
+   '24': 'x',
+    '25': 'y',
+    '26': 'z'
+}
+
+numbers = input().split()
+text = ''
+for number in numbers:
+    if number.isdigit():
+        text += numbers_to_letters[number]
+    else:
+        text += number
+        text += ' '
+print(text)
---
+++ task28.py
@@ -0,0 +1,8 @@
+from task25 import chru
+
+fraza = "grznuamn zngz cge sge tuz hk uhbouay gz loxyz atrkyy eua'xk jazin."
+chif = 'def'
+l = 'e'
+for i in range(1, 27):
+    chru(chif, i, l, fraza)
+    print('\n')
